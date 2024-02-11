b86270bf4e7ae87c7f66d529472b02b4f47682f9
dankren <124093473+Dankrenn@users.noreply.github.com>
2024-02-11 15:53:15 +0300

    1234

---
+++ dz1 vinforms/Form3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace dz1_vinforms
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Открываем диалог сохранения файла PDF
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+            saveFileDialog.Title = "Save PDF File";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Создаем файл PDF и записываем в него содержимое из TextBox
+                using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.Write(textBox1.Text);
+                    }
+                }
+
+                // Печать PDF через принтер
+                PrintDocument printDoc = new PrintDocument();
+                printDoc.DocumentName = saveFileDialog.FileName;
+                printDoc.PrintPage += new PrintPageEventHandler(printDoc_PrintPage);
+                printDoc.Print();
+            }
+        }
+
+        private void printDoc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            // Открываем файл PDF и печатаем его содержимое
+            string filePath = ((PrintDocument)sender).DocumentName;
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string text = sr.ReadToEnd();
+                    e.Graphics.DrawString(text, textBox1.Font, Brushes.Black, e.MarginBounds);
+                }
+            }
+        }
+    }
+
+}
---
+++ dz1 vinforms/Form4.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace dz1_vinforms
+{
+    public partial class Form4 : Form
+    {
+        private Label labelIntersection;
+        public Form4()
+        {
+            InitializeComponent();
+            InitializeChart();
+            InitializeLabel();
+        }
+        private void InitializeChart()
+        {
+            chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            chart.Dock = DockStyle.Fill;
+            chart.ChartAreas.Add(new System.Windows.Forms.DataVisualization.Charting.ChartArea());
+            this.Controls.Add(chart);
+        }
+
+        private void InitializeLabel()
+        {
+            labelIntersection = new Label();
+            labelIntersection.AutoSize = true;
+            labelIntersection.Location = new Point(10, 10); // Настройте координаты положения метки по вашему усмотрению
+            this.Controls.Add(labelIntersection);
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            // Создаем серии данных для графиков
+            chart.Series.Add("sin(x)");
+            chart.Series.Add("x^2");
+
+            // Задаем типы графиков
+            chart.Series["sin(x)"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart.Series["x^2"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // Задаем цвета графиков
+            chart.Series["sin(x)"].Color = Color.Blue;
+            chart.Series["x^2"].Color = Color.Red;
+
+            // Добавляем точки на графики
+            for (double x = -20; x <= 30; x += 0.4)
+            {
+                chart.Series["sin(x)"].Points.AddXY(x, Math.Sin(x));
+                chart.Series["x^2"].Points.AddXY(x, Math.Pow(x, 2));
+            }
+
+            // Определяем точку пересечения графиков
+            double intersectionX = FindIntersection();
+            double intersectionY = Math.Sin(intersectionX);
+
+            // Определяем точку пересечения графиков
+            DataPoint intersectionPointSin = chart.Series["sin(x)"].Points.FindByValue(intersectionY);
+            DataPoint intersectionPointXSquare = chart.Series["x^2"].Points.FindByValue(intersectionX);
+
+            if (intersectionPointSin != null && intersectionPointXSquare != null)
+            {
+                // Отмечаем точку пересечения на графиках
+                intersectionPointSin.Color = Color.Green;
+                intersectionPointXSquare.Color = Color.Green;
+            }
+
+            // Выводим координаты точки пересечения
+            label1.Text = string.Format("Точка пересечения: ({0}, {1})", intersectionX, intersectionY);
+
+        }
+
+        private double FindIntersection()
+        {
+            // Определяем точку пересечения графиков методом половинного деления
+            double epsilon = 0.0001; // Точность вычислений
+            double minX = -20;
+            double maxX = 10;
+
+            while (maxX - minX > epsilon)
+            {
+                double midX = (minX + maxX) / 2;
+                double sinValue = Math.Sin(midX);
+                double xSquareValue = Math.Pow(midX, 2);
+
+                if (Math.Abs(sinValue - xSquareValue) < epsilon)
+                    return midX;
+
+                if (sinValue < xSquareValue)
+                    minX = midX;
+                else
+                    maxX = midX;
+            }
+
+            return (minX + maxX) / 2;
+        }
+    }
+}
